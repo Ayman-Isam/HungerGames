1a795838b3ccb2a5bf3ea745067930be84d6951f
Ayman Isam <aymanisam26@gmail.com>
2024-01-29 18:27:57 +0200

    Moved ScanArena to an async task

--- src/main/java/me/cantankerousally/hungergames/commands/ScanArenaCommand.java
+++ src/main/java/me/cantankerousally/hungergames/commands/ScanArenaCommand.java
@@ -1,9 +1,6 @@
 package me.cantankerousally.hungergames.commands;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -49,67 +46,81 @@ public class ScanArenaCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (command.getName().equalsIgnoreCase("scanarena")) {
-            FileConfiguration config = getArenaConfig();
+            // Run the scanning process asynchronously
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+                FileConfiguration config = getArenaConfig();
 
-            if (!config.isSet("region.pos1.x") || !config.isSet("region.pos1.y") || !config.isSet("region.pos1.z")
-                    || !config.isSet("region.pos2.x") || !config.isSet("region.pos2.y") || !config.isSet("region.pos2.z")) {
-                sender.sendMessage(ChatColor.RED + "The arena region is not defined in the config!");
-                return true;
-            }
+                if (!config.isSet("region.pos1.x") || !config.isSet("region.pos1.y") || !config.isSet("region.pos1.z")
+                        || !config.isSet("region.pos2.x") || !config.isSet("region.pos2.y") || !config.isSet("region.pos2.z")) {
+                    sender.sendMessage(ChatColor.RED + "The arena region is not defined in the config!");
+                    return;
+                }
 
-            World world = plugin.getServer().getWorld(Objects.requireNonNull(config.getString("region.world")));
-            double pos1x = config.getDouble("region.pos1.x");
-            double pos1y = config.getDouble("region.pos1.y");
-            double pos1z = config.getDouble("region.pos1.z");
-            double pos2x = config.getDouble("region.pos2.x");
-            double pos2y = config.getDouble("region.pos2.y");
-            double pos2z = config.getDouble("region.pos2.z");
+                World world = plugin.getServer().getWorld(Objects.requireNonNull(config.getString("region.world")));
+                double pos1x = config.getDouble("region.pos1.x");
+                double pos1y = config.getDouble("region.pos1.y");
+                double pos1z = config.getDouble("region.pos1.z");
+                double pos2x = config.getDouble("region.pos2.x");
+                double pos2y = config.getDouble("region.pos2.y");
+                double pos2z = config.getDouble("region.pos2.z");
 
-            int minX = (int) Math.min(pos1x, pos2x);
-            int minY = (int) Math.min(pos1y, pos2y);
-            int minZ = (int) Math.min(pos1z, pos2z);
-            int maxX = (int) Math.max(pos1x, pos2x);
-            int maxY = (int) Math.max(pos1y, pos2y);
-            int maxZ = (int) Math.max(pos1z, pos2z);
+                int minX = (int) Math.min(pos1x, pos2x);
+                int minY = (int) Math.min(pos1y, pos2y);
+                int minZ = (int) Math.min(pos1z, pos2z);
+                int maxX = (int) Math.max(pos1x, pos2x);
+                int maxY = (int) Math.max(pos1y, pos2y);
+                int maxZ = (int) Math.max(pos1z, pos2z);
 
-            File chestLocationsFile = new File(plugin.getDataFolder(), "chest-locations.yml");
+                File chestLocationsFile = new File(plugin.getDataFolder(), "chest-locations.yml");
 
-            List<Location> chestLocations = new ArrayList<>();
-            List<Location> barrelLocations = new ArrayList<>();
-            List<Location> trappedChestLocations = new ArrayList<>();
-            for (int x = minX; x <= maxX; x++) {
-                for (int y = minY; y <= maxY; y++) {
-                    for (int z = minZ; z <= maxZ; z++) {
+                List<Location> chestLocations = new ArrayList<>();
+                List<Location> barrelLocations = new ArrayList<>();
+                List<Location> trappedChestLocations = new ArrayList<>();
+                int minChunkX = minX >> 4;
+                int maxChunkX = maxX >> 4;
+                int minChunkZ = minZ >> 4;
+                int maxChunkZ = maxZ >> 4;
+
+                for (int chunkX = minChunkX; chunkX <= maxChunkX; chunkX++) {
+                    for (int chunkZ = minChunkZ; chunkZ <= maxChunkZ; chunkZ++) {
                         assert world != null;
-                        Block block = world.getBlockAt(x, y, z);
-                        if (block.getType() == Material.CHEST) {
-                            chestLocations.add(block.getLocation());
-                        } else if (block.getType() == Material.BARREL) {
-                            barrelLocations.add(block.getLocation());
-                        } else if (block.getType() == Material.TRAPPED_CHEST) {
-                            trappedChestLocations.add(block.getLocation());
+                        Chunk chunk = world.getChunkAt(chunkX, chunkZ);
+                        for (int x = 0; x < 16; x++) {
+                            for (int z = 0; z < 16; z++) {
+                                for (int y = minY; y <= maxY; y++) {
+                                    Block block = chunk.getBlock(x, y, z);
+                                    if (block.getType() == Material.CHEST) {
+                                        chestLocations.add(block.getLocation());
+                                    } else if (block.getType() == Material.BARREL) {
+                                        barrelLocations.add(block.getLocation());
+                                    } else if (block.getType() == Material.TRAPPED_CHEST) {
+                                        trappedChestLocations.add(block.getLocation());
+                                    }
+                                }
+                            }
                         }
                     }
                 }
-            }
 
-            FileConfiguration chestLocationsConfig = new YamlConfiguration();
-            chestLocationsConfig.set("locations", chestLocations.stream()
-                    .map(Location::serialize)
-                    .collect(Collectors.toList()));
-            chestLocationsConfig.set("bonus-locations", barrelLocations.stream()
-                    .map(Location::serialize)
-                    .collect(Collectors.toList()));
-            chestLocationsConfig.set("mid-locations", trappedChestLocations.stream()
-                    .map(Location::serialize)
-                    .collect(Collectors.toList()));
-            try {
-                chestLocationsConfig.save(chestLocationsFile);
-                sender.sendMessage(ChatColor.GREEN + "Chest locations have been saved!");
-            } catch (IOException e) {
-                sender.sendMessage(ChatColor.RED + "Could not save chest locations to file!");
-            }
+                FileConfiguration chestLocationsConfig = new YamlConfiguration();
+                chestLocationsConfig.set("locations", chestLocations.stream()
+                        .map(Location::serialize)
+                        .collect(Collectors.toList()));
+                chestLocationsConfig.set("bonus-locations", barrelLocations.stream()
+                        .map(Location::serialize)
+                        .collect(Collectors.toList()));
+                chestLocationsConfig.set("mid-locations", trappedChestLocations.stream()
+                        .map(Location::serialize)
+                        .collect(Collectors.toList()));
+                try {
+                    chestLocationsConfig.save(chestLocationsFile);
+                    sender.sendMessage(ChatColor.GREEN + "Chest locations have been saved!");
+                } catch (IOException e) {
+                    sender.sendMessage(ChatColor.RED + "Could not save chest locations to file!");
+                }
+            });
+            return true;
         }
-        return true;
+        return false;
     }
 }
